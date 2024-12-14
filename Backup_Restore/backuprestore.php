51fb86724afa6909b33f7e203acced1f2bf0567c
Andrew Hennis <dawnandrew100@gmail.com>
2024-12-13 21:37:16 -0500

    PHP progress sorta

--- Backup_Restore/backuprestore.php
+++ Backup_Restore/backuprestore.php
@@ -1,9 +1,27 @@
 <?php
-
 function test() {
     echo "This is a test to make sure PHP is downloaded properly and works";
 }
 
-test()
+test();
 
+$url = "https://sampleapis.com/";
+$curl = curl_init($url);
+if($curl) {
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, True);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    echo $response;
+}
+else {
+    echo "Could not run";
+}
+if(curl_exec($curl) === false)
+{
+    echo 'Curl error: ' . curl_error($curl);
+}
+else
+{
+    echo 'Operation completed without any errors, you have the response';
+}
 ?>
